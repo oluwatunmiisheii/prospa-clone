7644b6ddd882f855f5ea0f9c24fa6c6d9f562e0b
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-02-10 00:53:05 +0100

    wip: auth layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   );
 }
 
